b004f409a8a9ddde1be553337e97ccfc30dca19c
Bipin Patel <bipin.patel@bluereeftech.com>
2021-01-12 10:54:44 +0530

    changes

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 const socketIO = require('socket.io');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 4334;
-const INDEX = '/index.html';
+const CONFIG = {
+  PORT: 4334
+}
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+app.use(cors({credentials: true, origin: true}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+// Create socketIO and wrap app server inside
+const server = http.Server(app);
 const io = socketIO(server);
+
+// Add middleware to handle post request for express
+const form = multer();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/static', express.static('static'));
+
+// Serve index.html for path '/', this is home path
+app.get('/', (req, res, next) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+// Serve index.html for path '/', this is home path
+app.post('/', (req, res, next) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+server.listen(CONFIG.PORT, () => {
+  console.log('Server is running at port: ' + CONFIG.PORT);
+});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
